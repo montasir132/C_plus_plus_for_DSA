a08d259eedfe004f772168bc909f10ba2e4eded8
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-11-23 23:41:54 +0600

    Add files via upload

---
+++ practice_problem/F_Digits_Summation.cpp
@@ -0,0 +1,12 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    long long int n,m;
+    cin>>n>>m;
+    long long int a=n%10,b=m%10;
+    long long int sum=a+b;
+    cout<<sum;
+    return 0;
+}
