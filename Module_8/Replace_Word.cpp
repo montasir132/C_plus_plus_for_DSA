bdd72e3a450487851365bdcf5a4e69de20fca0dd
Md. Montasir Alam <166697994+montasir132@users.noreply.github.com>
2025-12-03 00:09:47 +0600

    Add files via upload

--- Module_8/Replace_Word.cpp
+++ Module_8/Replace_Word.cpp
@@ -1,22 +1,72 @@
+// https://www.hackerrank.com/contests/final-exam-a-introduction-to-c-for-dsa-a-batch-08/challenges/replace-word
+
+// #include <bits/stdc++.h>
+// using namespace std;
+// int main()
+// {
+//     int t;
+//     cin >> t;
+//     while (t--)
+//     {
+
+//         string s, x;
+//         cin >> s >> x;
+//         int len_s = s.size();
+//         int len_x = x.size();
+//         for (int i = 0; i < len_s; i++)
+//         {
+//             int tmp = 1;
+//             if (i + len_x <= len_s)
+//             { // traversing
+//                 for (int j = 0; j < len_x; j++)
+//                 {
+//                     if (s[i + j] != x[j])
+//                     {
+//                         tmp = 0;
+//                         break;
+//                     }
+//                 }
+//             }
+//             else
+//                 tmp = 0;
+//             if (tmp)
+//             {
+//                 cout << "#";
+//                 i += len_x - 1;
+//             }
+//             else
+//             {
+//                 cout << s[i];
+//             }
+//         }
+//         cout << endl;
+//     }
+//     return 0;
+// }
+
 #include <bits/stdc++.h>
 using namespace std;
+
 int main()
 {
-    string str,x;
-    cin >> str>>x;
-    int len = str.length();
-    for (int i = 0; i < len; i++)
+    int t;
+    cin >> t;
+    while (t--)
     {
-        if (str[i] == x[i])
+        string s, x;
+        cin >> s >> x;
+        int x_len = x.length();
+        while (true)
         {
-            cout << "#";
-            i += 4;
+            int idx = s.find(x);
+            if (idx != -1)
+            {
+                s.replace(idx, x_len, "#");
+            }
+            else
+                break;
         }
-        else
-        {
-            cout << str[i];
-            // i++;
-        }
+        cout << s << endl;
     }
     return 0;
 }
